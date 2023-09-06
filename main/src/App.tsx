5daf42c7140e78a0fe07595513ad786ee94166bf
Jiacheng-coder <2253541907@qq.com>
2023-09-06 12:26:09 +0800

    Initial commit

--- main/src/App.tsx
+++ main/src/App.tsx
@@ -1,31 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { Tips } from 'common'
+import WujieReact from 'wujie-react'
 
 function App() {
-  const [count, setCount] = useState(0)
   console.log('EcosHttp', Tips)
 
   return (
     <>
-      <div>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
+      <section>
+        <h1>主应用</h1>
+      </section>
+      <section>
+        <h2>子应用</h2>
+        <WujieReact name='vue3' url='http://localhost:5174/' />
+        <WujieReact name='react' url='http://localhost:5175/' />
+      </section>
     </>
   )
 }
